60fa077b90efb5360905feedbd3ea3c2bb7f6916
unknown <dungnguyenhuy95@gmail.com>
2022-02-25 17:40:19 +0700

    'animation_home_button'

--- src/screens/short/ShortVideoScreen.js
+++ src/screens/short/ShortVideoScreen.js
@@ -1,95 +1,85 @@
-// import React, { useEffect, useState } from 'react'
-// import { View, Text, TextInput, SafeAreaView, KeyboardAvoidingView, TouchableOpacity, Dimensions } from 'react-native'
-// import { setName, setAge } from '../../redux/actions'
-// import ViewImage from '../../component/ViewImage'
-// import { useSelector, useDispatch } from 'react-redux'
-// const { width, height } = Dimensions.get('window')
+import React, { useState, useRef } from 'react';
+import { Animated, PanResponder, View, Image, StyleSheet, Text, Dimensions } from 'react-native';
 
-// export default function ShortVideoScreen({ navigation }) {
-//     const user = useSelector(state => state.userReducer)
-//     const [showImg, setShowImg] = useState(false)
-//     const dispatch = useDispatch()
+const { width, height } = Dimensions.get('screen')
+const spaceWidth = width / 2 - 50
+const spaceHeight = height / 2 - 150
 
-//     useEffect(() => {
-//         // console.log('navigation')
-//         return () => {
-//             console.log('back navigation')
-//         }
-//     }, [navigator])
-//     return (
-//         <SafeAreaView style={{ flex: 1}}>
-//             <KeyboardAvoidingView style={{ flex: 1 }}
-//                 behavior="padding"
-//                 enabled >
-//                 <Text>ShortVideoScreen</Text>
-//                 <Text>{user.name}</Text>
-//                 <TextInput style={{ borderWidth: 0.5 }}
-//                     placeholder='Nhập tên người dùng'
-//                     onChangeText={(text) => {
-//                         dispatch(setName(text))
-//                     }}
-//                 />
-//                 <TouchableOpacity onPress={() => setShowImg(true)}><Text style={{ color: 'black' }}>ShowImage</Text></TouchableOpacity>
-//                 <TouchableOpacity onPress={() => navigation.goBack()}><Text style={{ color: 'black' }}>BackHome</Text></TouchableOpacity>
-//                 <View style={{ position: 'absolute', top: (height - 300) / 2, left:(width-300)/2}}>
-//                     {showImg && <ViewImage callBack={() => setShowImg(false)} />}
-//                 </View>
-//             </KeyboardAvoidingView>
-//         </SafeAreaView>
-//     )
-// }
+const ImageScreen = () => {
 
+  const pan = useRef(new Animated.ValueXY({ x: spaceWidth, y: -spaceHeight })).current;
 
-import React, { useRef } from "react";
-import { Animated, View, StyleSheet, PanResponder, Text } from "react-native";
-
-const App = () => {
-  const pan = useRef(new Animated.ValueXY()).current;
   const panResponder = useRef(
     PanResponder.create({
       onMoveShouldSetPanResponder: () => true,
-      onPanResponderMove: Animated.event([
-        null,
-        { dx: pan.x, dy: pan.y }
-      ]),
+      onPanResponderGrant: () => {
+        pan.setOffset({
+          x: pan.x._value,
+          y: pan.y._value
+        });
+      },
+      onPanResponderMove: Animated.event(
+        //moving
+        [
+          null, //raw event arg ignored
+          { dx: pan.x, dy: pan.y } // dx, dy id gestureState
+        ],
+        { useNativeDriver: false }
+      ),
       onPanResponderRelease: () => {
-        Animated.spring(pan, { toValue: { x: 100, y: 100 }, useNativeDriver:true }).start();
+        // call when stop moving = 'release your finger'
+        console.log(pan.x._value, spaceHeight)
+        pan.flattenOffset();
+        Animated.timing(pan,
+          {
+            toValue: {
+              x: pan.x._value > 0 ? spaceWidth : -spaceWidth,
+              y: Math.abs(pan.y._value) < spaceHeight ? pan.y._value : pan.y._value > 0 ? spaceHeight : -spaceHeight
+            },
+            useNativeDriver: false,
+            duration: 500,
+            delay: 200
+          }).start();
       }
     })
   ).current;
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.titleText}>Drag & Release this box!</Text>
+    <View style={style.mainView}
+      onStartShouldSetResponderCapture={() => { return false }}>
       <Animated.View
         style={{
+          zIndex: 1,
           transform: [{ translateX: pan.x }, { translateY: pan.y }]
         }}
         {...panResponder.panHandlers}
       >
-        <View style={styles.box} />
+        <Image
+          style={style.moviePoster_posterStyle}
+          source={require("../../assets/img/nu_sinh.png")}
+        />
       </Animated.View>
+      <Text>hello</Text>
     </View>
-  );
-}
+  )
 
-const styles = StyleSheet.create({
-  container: {
+
+};
+
+export default ImageScreen;
+
+
+const style = StyleSheet.create({
+  mainView: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
   },
-  titleText: {
-    fontSize: 14,
-    lineHeight: 24,
-    fontWeight: "bold"
-  },
-  box: {
-    height: 150,
-    width: 150,
-    backgroundColor: "blue",
-    borderRadius: 5
+  moviePoster_posterStyle: {
+    resizeMode: "cover",
+    width: 100,
+    height: 130
   }
 });
 
-export default App;
+
--- src/screens/use/UserScreen.js
+++ src/screens/use/UserScreen.js
@@ -1,22 +1,52 @@
-import React, {useState, useRef} from 'react';
-import { Animated, PanResponder, View, Image, StyleSheet } from 'react-native';
+// import React, { useEffect, useState } from 'react'
+// import { View, Text, TextInput, SafeAreaView, KeyboardAvoidingView, TouchableOpacity, Dimensions } from 'react-native'
+// import { setName, setAge } from '../../redux/actions'
+// import ViewImage from '../../component/ViewImage'
+// import { useSelector, useDispatch } from 'react-redux'
+// const { width, height } = Dimensions.get('window')
 
-const style = StyleSheet.create({
-  mainView: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },  
-  moviePoster_posterStyle: {
-    resizeMode: "cover",
-    width:100,
-    height:130
-  }
-});
+// export default function ShortVideoScreen({ navigation }) {
+//     const user = useSelector(state => state.userReducer)
+//     const [showImg, setShowImg] = useState(false)
+//     const dispatch = useDispatch()
 
-const ImageScreen = () => {
+//     useEffect(() => {
+//         // console.log('navigation')
+//         return () => {
+//             console.log('back navigation')
+//         }
+//     }, [navigator])
+//     return (
+//         <SafeAreaView style={{ flex: 1}}>
+//             <KeyboardAvoidingView style={{ flex: 1 }}
+//                 behavior="padding"
+//                 enabled >
+//                 <Text>ShortVideoScreen</Text>
+//                 <Text>{user.name}</Text>
+//                 <TextInput style={{ borderWidth: 0.5 }}
+//                     placeholder='Nhập tên người dùng'
+//                     onChangeText={(text) => {
+//                         dispatch(setName(text))
+//                     }}
+//                 />
+//                 <TouchableOpacity onPress={() => setShowImg(true)}><Text style={{ color: 'black' }}>ShowImage</Text></TouchableOpacity>
+//                 <TouchableOpacity onPress={() => navigation.goBack()}><Text style={{ color: 'black' }}>BackHome</Text></TouchableOpacity>
+//                 <View style={{ position: 'absolute', top: (height - 300) / 2, left:(width-300)/2}}>
+//                     {showImg && <ViewImage callBack={() => setShowImg(false)} />}
+//                 </View>
+//             </KeyboardAvoidingView>
+//         </SafeAreaView>
+//     )
+// }
 
-  const pan = useRef(new Animated.ValueXY()).current;
+
+import React, { useRef } from "react";
+import { Animated, View, StyleSheet, PanResponder, Text, Dimensions } from "react-native";
+
+const { width, height } = Dimensions.get('screen')
+
+const App = () => {
+  const pan = useRef(new Animated.ValueXY({ x: width / 2 - 75, y: -height / 2 + 100 })).current;
 
   const panResponder = useRef(
     PanResponder.create({
@@ -31,8 +61,7 @@ const ImageScreen = () => {
         [
           null,
           { dx: pan.x, dy: pan.y }
-        ],
-        {useNativeDriver: false}
+        ]
       ),
       onPanResponderRelease: () => {
         pan.flattenOffset();
@@ -40,24 +69,38 @@ const ImageScreen = () => {
     })
   ).current;
 
-    return (
-      <View style={style.mainView}
-        onStartShouldSetResponderCapture={() => {return false}}>
-        <Animated.View
-          style={{
-            transform: [{ translateX: pan.x }, { translateY: pan.y }]
-          }}
-          {...panResponder.panHandlers}
-        >
-          <Image
-              style={style.moviePoster_posterStyle}
-              source={require("../../assets/img/nu_sinh.png")}
-          />
-        </Animated.View>
-      </View>
-    )
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titleText}>Drag this box!</Text>
+      <Animated.View
+        style={{
+          transform: [{ translateX: pan.x }, { translateY: pan.y }]
+        }}
+        {...panResponder.panHandlers}
+      >
+        <View style={styles.box} />
+      </Animated.View>
+    </View>
+  );
+}
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  titleText: {
+    fontSize: 14,
+    lineHeight: 24,
+    fontWeight: "bold"
+  },
+  box: {
+    height: 150,
+    width: 150,
+    backgroundColor: "blue",
+    borderRadius: 5
+  }
+});
 
-};
-
-export default ImageScreen;
+export default App;
